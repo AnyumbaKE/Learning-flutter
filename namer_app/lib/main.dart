12b68e3532b87ae004977d0152d9eba27d97099e
AnyumbaKE <esanyumba@gmail.com>
2024-02-18 16:23:48 +0300

    refactor

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -44,7 +44,7 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(pair.asLowerCase),
+          BigCard(pair: pair),
 
           ElevatedButton(
             onPressed:(){
@@ -57,3 +57,18 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
+  }
+}
+
